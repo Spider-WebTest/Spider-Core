4723d6963900c71e7344e16ddcbc98954826c616
benmiled <benmiledaymen@gmail.com>
2018-02-14 19:06:30 +0100

    commit first hello world command to test travis ci.

---
+++ Spider-cli/Spider-cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spider_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
